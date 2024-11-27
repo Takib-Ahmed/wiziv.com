a59914b98d9f0aca313c8f1c5521f683a5fdf70f
Takib-Ahmed <satakib300@gmail.com>
2024-11-27 16:52:52 +0600

    Partner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ export default function Hero(){
     return (
       <>
       <div className=" Hero  w-full">
-<div className="grid gap-5 p-10 px-14">
+<div className="grid gap-5 py-20 px-14">
   <div><p className="  text-Highlight  text-xl">AI SDRs (aiDRs)</p></div>
   <div><h1 className=" text-5xl text-white"><font className="font-bold">More</font> <i>leads,</i><br /> <font className="font-bold">less </font><i>people.</i></h1></div>
   <div><p className="  text-gray-300 font-thin">Train an aiDR on your ICP and messaging matrix. <br /> Activate it on a patch. It will send personalized <br /> sequences to every target contact.</p></div>
-  <div><Button color="primary" variant="shadow">
-        Sign up for the Beta 
+  <div><Button color="primary" variant="shadow" radius="full" className=" px-5 text-black">
+        Sign up for the Beta <img src="/public/Hero/Group.png" alt="" className=" w-3.5 pt-1"/>
       </Button>  </div>
 </div>
       </div>
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 export default function Partners(){
+    const [Partnerlist] = useState({
+        title:'our trusted partners',
+        partners:[
+            ' /partners/BackHub logo.png',
+            ' /partners/CableLabs logo.png',
+            ' /partners/DBS logo.png','/public/partners/EasyEuro logo.png'
+            ,'/partners/AMD logo.png'
+            
+        ]
+    })
     return (
        <>
+<center className=" p-7  ">
+    <p className="title text-Highlight font-light">{Partnerlist.title.toUpperCase()}</p> <br />
+   <div className={`grid sm:flex justify-center w-full     md:gap-8 lg:gap-14 ` + `${window.innerWidth<730 ? 'gap-[0.02rem]':'gap-5'}`}> 
+   {
+        Partnerlist.partners.map((value,key)=>(
+            <img key={key} src={value}/>
+
+            
+        ))
+    }
+   </div>
+</center>
+
        </> 
     );
 }
