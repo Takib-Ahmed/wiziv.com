d595864569993623ae3b47e27dd8e0e41ab1b5cf
Takib-Ahmed <satakib300@gmail.com>
2024-11-27 23:24:55 +0600

    Footeradded

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 export default function Footer(){
     return (
         <>
+<div className="flex justify-between bg-[#07292F] p-5 px-16 ">
+    <div className="brand">
+        <img src="/public/Footer/Logo.png" alt="" />
 
+    </div>
+    <div className="copyright">
+        <i>Copyright © 2023 Wizia. All rights reserved.</i>
+    </div>
+</div>
     
         </>
     );
